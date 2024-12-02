a62a35bba7148666b0cfd9a7c4871e8b09a086b3
Miller Raycell <millerraycell@gmail.com>
2024-12-01 22:37:01 -0400

    deno part 2

--- day-01/deno/main.ts
+++ day-01/deno/main.ts
@@ -1,26 +1,48 @@
 import file from './entries.json' with { type: "json" };
 
-function calculateDistance() {
-  const firstList: Array<number> = [];
-  const secondList: Array<number> = [];
-  let sum = 0;
+function parseDistances(side: string): Array<number> {
+  const listDistances: Array<number> = [];
 
   file.forEach(entry => {
-    firstList.push(entry[0]);
-    secondList.push(entry[1]);
+    if (side === 'right') {
+      listDistances.push(entry[0]);
+    } else {
+      listDistances.push(entry[1])
+    }
   });
 
-  firstList.sort();
-  secondList.sort();
+  listDistances.sort();
 
-  for (let i = 0; i < firstList.length; i++) {
-    sum += Math.abs(firstList[i] - secondList[i]);
+  return listDistances;
+}
+
+function calculateDistance(): void {
+  const rightList = parseDistances('right');
+  const leftList = parseDistances('left');
+  let sum = 0;
+
+  for (let i = 0; i < rightList.length; i++) {
+    sum += Math.abs(rightList[i] - leftList[i]);
   }
 
   console.log("Total difference is: ", sum);
 }
 
+function calculateFrequencyRight(): void {
+  const rightList = parseDistances('right');
+  const leftList = parseDistances('left');
+  let sum = 0;
+
+  rightList.forEach(rightDistance => {
+    const filteredDistances = leftList.filter(leftDistance => rightDistance === leftDistance);
+    sum += rightDistance * filteredDistances.length;
+  });
+
+  console.log("The similarity is: ", sum);
+}
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   calculateDistance();
+  calculateFrequencyRight();
 }
